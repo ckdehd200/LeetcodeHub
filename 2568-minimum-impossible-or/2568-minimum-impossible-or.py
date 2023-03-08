b6e9ad33081cd5b2fa5a758bafa0702cd89b8ebe
Gomm <69075475+ckdehd200@users.noreply.github.com>
2023-03-09 00:24:47 +0900

    Time: 1005 ms (5.01%), Space: 32 MB (7.41%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.py
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.py
@@ -1,4 +1,4 @@
 class Solution:
     
     def minImpossibleOR(self, nums: List[int]) -> int:
-        return min([1<<e for e in range(40) if 1<<e not in {k:1 for k in nums}.keys()])
+        return min([1<<e for e in range(40) if 1<<e not in set(nums)])
